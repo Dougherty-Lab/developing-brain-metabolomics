6fef29f0e0ce95cf9747304b38ed8dbf41f60201
Emma Jones <85246122+emmafjones@users.noreply.github.com>
2025-05-02 09:58:04 -0500

    edit gitignore and add bin/docker

---
+++ bin/docker/run_rstudio.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=rstudio
+#SBATCH --mem=200GB
+#SBATCH --cpus-per-task=2
+#SBATCH --time=2-00:00:00
+
+
+# configure spack
+. /ref/jdlab/software/spack/share/spack/setup-env.sh
+
+# load singularity
+spack load singularityce
+
+# print ssh address
+host=$(hostname)
+
+echo -e "
+        SSH tunnel to $host
+        ssh $USER@login.htcf.wustl.edu -N -L 8787:$host:8787
+
+        Open in browser: http://localhost:8787
+"
+# execute container
+singularity exec \
+   -B /tmp:/var/lib/rstudio-server \
+   -B /tmp:/var/run/rstudio-server \
+   -B /scratch/jdlab/emma/fetal_brain_hormone_analyis \
+   rstudio-myt1l-pilot_1.0.9.sif \
+   rserver \
+     --server-user=$USER
+
+# expected output: INFO:    Converting SIF file to temporary sandbox...
